af52b04effff53d2f8ba06e954c6f023a8eab7f2
YorMuhammad <153153430+yormuhamad@users.noreply.github.com>
2024-05-16 13:41:17 +0500

    Update task4.dart

--- task4.dart
+++ task4.dart
@@ -1,9 +1,9 @@
 void main() {
-  List<int> numbers = [1, 3, 5, 7, 9];
-  double average = numbers.reduce((a, b) => a + b) / numbers.length;
+  List<int> raqamho = [1, 3, 5, 7, 9];
+  double miyona = raqamho.reduce((a, b) => a + b) / raqamho.length;
   
-  List<int> aboveAverage = numbers.where((number) => number > average).toList();
+  List<int> miyonab = raqamho.where((raqam) => raqam > miyona).toList();
   
-  print('Қимати миёнаи массив: $average');
-  print('Элементҳои массив аз ҳисоби миёна калонтар: $aboveAverage');
+  print('Қимати миёнаи массив: $miyona');
+  print('Элементҳои массив аз ҳисоби миёна калонтар: $miyonab');
 }
