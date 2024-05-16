27dd8ee211dc88d82dabf138045c0d3c23bfff02
YorMuhammad <153153430+yormuhamad@users.noreply.github.com>
2024-05-16 13:39:22 +0500

    Update task3.dart

--- task3.dart
+++ task3.dart
@@ -3,18 +3,18 @@ import 'dart:io';
 void main() {
   print('Як пайдарпаии ададҳои бо сифр анҷомёфтаро ворид кунед:');
   
-  int number;
-  bool found = false;
+  int raqam;
+  bool yoftan = false;
 
-  while (!found) {
-    number = int.parse(stdin.readLineSync()!);
-    if (number == 0) {
+  while (!yoftan) {
+    raqam = int.parse(stdin.readLineSync()!);
+    if (raqam == 0) {
       print('Пайдарпай бо сифр қатъ карда мешавад.');
       break;
     }
-    if (number < 0) {
-      print('Аввалин рақами манфӣ дар пайдарпаӣ: $number');
-      found = true;
+    if (raqam < 0) {
+      print('Аввалин рақами манфӣ дар пайдарпаӣ: $raqam');
+      yoftan = true;
     }
   }
 }
