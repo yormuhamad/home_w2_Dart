7d724a6e49aaf20851452b10f7ab9e96b8df87bc
YorMuhammad <153153430+yormuhamad@users.noreply.github.com>
2024-05-16 13:38:57 +0500

    Update task2.dart

--- task2.dart
+++ task2.dart
@@ -1,23 +1,23 @@
 void main() {
   int a = 3;
   int b = 7;
-  int product = multiplyWithoutOperator(a, b);
-  print('Ҳосили зарби  $a ва $b: $product аст');
+  int pr = zioydKardan(a, b);
+  print('Ҳосили зарби  $a ва $b: $pr аст');
 }
 
-int multiplyWithoutOperator(int a, int b) {
-  int result = 0;
+int zioydKardan(int a, int b) {
+  int natija = 0;
  // Агар b мусбат бошад, пас a b маротиба илова кунед
   if (b > 0) {
     for (int i = 0; i < b; i++) {
-      result += a;
+      natija += a;
     }
   }
  // Агар b манфӣ бошад, пас a ба (-b) зарб кунед
   else if (b < 0) {
     for (int i = 0; i > b; i--) {
-      result -= a;
+      natija -= a;
     }
   }
-  return result;
+  return natija;
 }
